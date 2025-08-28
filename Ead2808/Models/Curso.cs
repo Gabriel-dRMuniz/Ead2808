e441a6613e7e64ad98f384196a770e23e2772e05
Gabriel-dRMuniz <gabriel.r.muniz7@aluno.senai.br>
2025-08-28 16:38:35 -0300

    Add files via upload

---
+++ Ead2808/Models/Curso.cs
@@ -0,0 +1,9 @@
+namespace Ead2808.Models
+{
+    public class Curso
+    {
+        public int Id { get; set; }
+        public string NomeCurso { get; set; }
+
+    }
+}
---
+++ Ead2808/UI/Program.cs
@@ -0,0 +1,9 @@
+﻿using Ead2808.Data;
+using Ead2808.Service;
+
+var ms = @"Server=localhost\SQLEXPRESS;Database=NomeDoDBAqui;Trusted_Connection=True;";
+
+Db.ConnectionString = ms;
+
+var service = new MatriculaService();
+service.Executar();
