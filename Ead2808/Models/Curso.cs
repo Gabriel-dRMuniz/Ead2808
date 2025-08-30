0d8e0d33bc30e0b1e9d53d3a2b63daccbc8e2e52
Gabriel-dRMuniz <gabriel.r.muniz7@aluno.senai.br>
2025-08-29 23:12:18 -0300

    Ead2808 directory

--- Ead2808/Models/Curso.cs
+++
@@ -1,9 +0,0 @@
-namespace Ead2808.Models
-{
-    public class Curso
-    {
-        public int Id { get; set; }
-        public string NomeCurso { get; set; }
-
-    }
-}
--- Ead2808/UI/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Ead2808.Data;
-using Ead2808.Service;
-
-var ms = @"Server=localhost\SQLEXPRESS;Database=NomeDoDBAqui;Trusted_Connection=True;";
-
-Db.ConnectionString = ms;
-
-var service = new MatriculaService();
-service.Executar();
